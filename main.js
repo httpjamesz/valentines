15f12dadca7341a008cd355f3b30c031523ff573
James <142896086+httpjamesz@users.noreply.github.com>
2024-02-13 15:56:42 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+
+var audio = document.createElement("AUDIO")
+document.body.appendChild(audio);
+audio.src = "RiverFlowsInYou.mp3"
+
+document.body.addEventListener("mousemove", function () {
+    audio.play()
+})
