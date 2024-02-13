2c01ac255816639db0bff565ac372109d591d37c
James <142896086+httpjamesz@users.noreply.github.com>
2024-02-13 16:10:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
     var btn_reset = $("#reset");
     var noClickCount = 0;
   
-    // Get the audio element
     var bgAudio = document.getElementById("bgAudio");
     
     envelope.click(function() {
@@ -19,36 +18,26 @@ $(document).ready(function() {
   
     function open() {
       envelope.addClass("open").removeClass("close");
-      // Show the response buttons
       $('.response-buttons').show();
-      // Show the "Close" button
       $('#reset').show();
-      // Hide the "Open" button
       $('#open').hide();
     }
   
     function close() {
       envelope.addClass("close").removeClass("open");
-      // Hide the response buttons
       $('.response-buttons').hide();
-      // Show the "Open" button
       $('#open').show();
-      // Hide the "Close" button
       $('#reset').hide();
     }
   
-    // Event listener for the "Yes" button
     $('.yes-btn').click(function() {
-      // Change the text
       $('.words.line2').text("Thank you love, kita na");
       $('.words.line3').text("tayo tomorrow! :))");
-      // Wait for 5 seconds before redirecting
       setTimeout(function() {
         window.location.href = "flower.html";
       }, 1500);
     });
   
-    // Array of possible texts for line2 and line3
     var randomTexts = [
       "Bakit naman ayaw mo?",
       "Kumukulit ka na love!",
@@ -60,18 +49,14 @@ $(document).ready(function() {
       "PUMAYAG KA NA KASI!!"
     ];
   
-    // Event listener for the "No" button
     $('.no-btn').click(function() {
-      if (noClickCount < 5) {
-        // Choose a random text from the array
+      if (noClickCount < 3) {
         var randomIndex = Math.floor(Math.random() * randomTexts.length);
         var randomText = randomTexts[randomIndex];
-        // Change line2 and line3 texts to the random text
         $('.words.line2').text(randomText);
         $('.words.line3').text("");
         noClickCount++;
       } else {
-        // Hide the "No" button and change line2 and line3 texts
         $('.no-btn').hide();
         var finalText = "Ayan, bawal ka na tumanggi!";
         $('.words.line2').text(finalText);
@@ -79,18 +64,12 @@ $(document).ready(function() {
       }
     });
   
-    // Event listener for the "Play Music" button
     $('#musicBtn').click(function() {
-      // Check if audio is paused
       if (bgAudio.paused) {
-        // Play the audio
         bgAudio.play();
-        // Change button text to "Pause Music"
         $(this).text("Pause Music");
       } else {
-        // Pause the audio
         bgAudio.pause();
-        // Change button text to "Play Music"
         $(this).text("Play Music");
       }
     });
